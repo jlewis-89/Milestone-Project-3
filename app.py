bd5cb2d21992b79cec83641b98f0f99bab53dda2
jlewis-89 <jlewis21689@live.com>
2024-05-06 13:17:29 +0100

    Bug Fixes to edit & replaced lost script file

--- app.py
+++ app.py
@@ -90,23 +90,6 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/edit_user/<username_id>", methods=["GET", "POST"])
-def edit_user(username):
-    # username = mongo.db.ideas.find_one({"_id": ObjectId(users_id)})
-    if request.method == "POST":
-        submit = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.user.update({"username": username}, submit)
-        flash("Profile Successfully Updated")
-        session["user"] = request.form.get("username").lower()
-        return redirect(url_for("profile", username=session["user"]))
-
-    user = mongo.db.user.find_one({"username": username})
-    return render_template("edit_user.html", user=user)
-
-
 @app.route("/logout")
 def logout():
     # remove user from session cookie
@@ -139,11 +122,12 @@ def add_ideas():
     return render_template("add_ideas.html", ideas=ideas)
 
 
-@app.route("/edit_ideas/<ideas_id>", methods=["GET", "POST"])
-def edit_ideas(ideas_id):
-    ideas = mongo.db.ideas.find_one({"_id": ObjectId(ideas_id)})
-
+@app.route("/edit_ideas/<idea_id>", methods=["GET", "POST"])
+def edit_ideas(idea_id):
+    print(idea_id)
+    idea = mongo.db.ideas.find_one({"_id": ObjectId(idea_id)})
     if request.method == "POST":
+        print("POST received")
         submit = {
             "title": request.form.get("title"),
             "description": request.form.get("description"),
@@ -151,12 +135,14 @@ def edit_ideas(ideas_id):
             "invest": request.form.get("invest"),
             "created_by": session["user"]
         }
-        mongo.db.ideas.update_one({"_id": ObjectId(ideas_id)}, submit)
+        mongo.db.ideas.replace_one({"_id": ObjectId(idea_id)}, submit)
         flash("Idea Successfully Updated")
+        print("redirect")
         return redirect(url_for("get_ideas"))
 
     ideas = mongo.db.ideas.find().sort("ideas", 1)
-    return render_template("edit_ideas.html", ideas=ideas)
+    print("render template", idea)
+    return render_template("edit_ideas.html", idea=idea)
 
 
 @app.route("/delete_ideas/<ideas_id>")
@@ -164,10 +150,6 @@ def delete_ideas(ideas_id):
     mongo.db.ideas.delete_one({"_id": ObjectId(ideas_id)})
     flash("Idea Successfully Deleted")
     return redirect(url_for("get_ideas"))
-
-
-def myfunc():
-    return None
 
 
 if __name__ == "__main__":
